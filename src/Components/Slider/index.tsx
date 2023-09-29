31dd697afa73f9c80ecd153f5077e94d94b66fc3
M.A.Felegari <M.A.Felegari@hotmail.com>
2023-09-29 23:04:37 +0330

    carousel place created

---
+++ src/Components/Slider/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import "index.css";
+import Carousel from "nuka-carousel";
+import sahneZaniPoster from "../../Assets/images/sahne-zani-poster.jpg";
+import sahneZaniPoster2 from "../../Assets/images/sahne-zani-poster2.jpg";
+
+interface SliderImageInfo {
+    src: string
+    alt: string
+}
+
+interface SliderProps {
+    images: SliderImageInfo[],
+}
+
+function Slider({images}: SliderProps) {
+    return (
+        <Carousel className="slider">
+            {images.map(image => (
+                <img src={image.src} alt={image.alt} className="image"/>
+            ))}
+        </Carousel>
+    );
+}
+
+export default Slider;
--- src/Screens/HomeScreen.tsx
+++ src/Screens/HomeScreen.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import HomeHeader from "../Components/header";
+import sahneZaniPoster from "../Assets/images/sahne-zani-poster.jpg";
+import sahneZaniPoster2 from "../Assets/images/sahne-zani-poster2.jpg";
+import Carousel, {ScrollMode} from "nuka-carousel";
 
 function HomeScreen() {
     return (
         <div>
             <HomeHeader/>
+            <div style={{backgroundColor:'var(--c-primary-light)',marginTop:15}}>
+                <Carousel style={{height:"30vh"}} autoplay={true} autoplayInterval={5000}>
+                    <img src={sahneZaniPoster} alt="1" style={{width:'100%',height:'30vh',translate:"translateY(50)",objectFit:'contain'}}/>
+                    <img src={sahneZaniPoster2} alt="2" style={{width:'100%',height:'30vh',objectFit:'contain'}}/>
+                    <img src={sahneZaniPoster} alt="3" style={{width:'100%',height:'30vh',objectFit:'contain'}}/>
+                </Carousel>
+            </div>
         </div>
     );
 }
