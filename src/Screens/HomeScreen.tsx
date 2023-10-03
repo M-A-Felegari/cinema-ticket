16bc2a64b8fa5f75879e64b64a6a5d1c353ad42b
M.A.Felegari <M.A.Felegari@hotmail.com>
2023-10-03 22:20:41 +0330

    tabs created

--- src/Screens/HomeScreen.tsx
+++ src/Screens/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import HomeHeader from "../Components/header";
 import sahneZaniPoster from "../Assets/images/sahne-zani-poster.jpg";
 import sahneZaniPoster2 from "../Assets/images/sahne-zani-poster2.jpg";
@@ -18,6 +18,8 @@ import CarouselSlide from "../Components/Carousel/CarouselSlide";
 import {RiFireFill} from "react-icons/ri";
 
 function HomeScreen() {
+
+    const [activeTab,setActiveTab] = useState(0)
 
     const slides = [
         {
@@ -55,6 +57,20 @@ function HomeScreen() {
             src: suspectXMiniPoster,
         }]
 
+    const tabs = [
+        {
+            id: 0,
+            title: "Today shows"
+        },
+        {
+            id: 1,
+            title: "Tomarrow"
+        },
+        {
+            id: 2,
+            title: "this week"
+        }
+    ]
     return (
         <main className="HomeScreen">
             <div className="container">
@@ -62,7 +78,7 @@ function HomeScreen() {
                 <section className="carouselContainer">
                     <Swiper className="carousel" pagination={true} navigation={true} autoplay={true}
                             modules={[Navigation, Autoplay, Pagination]} centeredSlides={true} spaceBetween={5}
-                            loop={true}
+                            loop={true} slide
                     >
                         {slides.map(slide => (
                             <SwiperSlide>
@@ -70,23 +86,54 @@ function HomeScreen() {
                             </SwiperSlide>
                         ))}
                     </Swiper>
+
                 </section>
             </div>
             <section className="sliderContainer">
-                    <p>
-                        <RiFireFill style={{color: 'var(--c-secondary)'}}/> New Released
-                    </p>
-                        <Swiper
-                            className="slider"
-                            slidesPerView={3}
-                            spaceBetween={15}
-                        >
-                            {newReleases.map(newRelease => (
-                                <SwiperSlide key={newRelease.id}>
-                                    <img src={newRelease.src} alt="newRelease"/>
-                                </SwiperSlide>
-                            ))}
-                        </Swiper>
+                <p>
+                    <RiFireFill style={{color: 'var(--c-secondary)'}}/> New Released
+                </p>
+                <Swiper
+                    className="slider"
+                    slidesPerView={3}
+                    spaceBetween={20}
+                    breakpoints={{
+                        500: {
+                            slidesPerView: 4
+                        },
+                        700: {
+                            slidesPerView: 5
+                        },
+                        900: {
+                            slidesPerView: 6
+                        },
+                        1100: {
+                            slidesPerView: 7
+                        }
+                    }}
+                >
+                    {newReleases.map(newRelease => (
+                        <SwiperSlide key={newRelease.id}>
+                            <img src={newRelease.src} alt="newRelease"/>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </section>
+            <section className="shows">
+                <div className="tabs">
+                    <div className="tabsContainer">
+                        {tabs.map(tab=>(
+                            <button
+                                key={tab.id}
+                                className={`tab ${activeTab === tab.id ? "active":null}`}
+                                onClick={()=>setActiveTab(tab.id)}
+                            >
+                                {tab.title}
+                                {/*TODO: when i contact to api i should put onclick method seprate and call contact with api in it*/}
+                            </button>
+                        ))}
+                    </div>
+                </div>
             </section>
         </main>
     );
