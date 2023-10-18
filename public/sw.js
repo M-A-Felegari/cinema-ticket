9af54c7eb754d909c91155bee95feff310e1764a
M.A.Felegari <M.A.Felegari@hotmail.com>
2023-10-18 15:10:56 +0330

    sw added

---
+++ public/sw.js
@@ -0,0 +1,35 @@
+const cacheData = "cacheV1";
+this.addEventListener("install", (event) => {
+    event.waitUntil(() => {
+        caches.open(cacheData)
+            .then((cache) => {
+                cache.addAll([
+                    '/static/js/bundle.js',
+                    // 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;800&display=swap',
+                    '/static/media/Joker-banner.250f2107124ba195735a.jpg',
+                    '/static/media/sahne-zani-poster2.266ae6b373d9765ac9aa.jpg',
+                    '/static/media/sahne-zani-poster.2a5d87f6832df3fcbc23.jpg\n',
+                    '/static/media/sahne-zani-mini-poster.04c236be013328c62200.jpg',
+                    '/static/media/JokerMiniPoster.8a5ba32dcb6feabae353.webp',
+                    '/static/media/suspect-x-mini-poster.d8bbbd1175d8ec1edf68.jpg',
+                    '/static/media/login_character.329499102f74e48a3e9fd70ed181889b.svg',
+                    '/index.html',
+                    '/',
+                    '/tickets',
+                    '/profile'
+                ])
+            })
+    })
+});
+
+this.addEventListener("fetch", (event) => {
+    if (!navigator.onLine){
+        event.respondWith(()=>{
+            caches.match(event.request)
+                .then((response)=>{
+                    if (response)
+                        return response;
+                })
+        })
+    }
+})
